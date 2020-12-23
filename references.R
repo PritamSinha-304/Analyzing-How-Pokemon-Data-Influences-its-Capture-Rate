178af5471f6618fc94c5e35702727aa05ebe03db
PritamSinha-304 <73048152+PritamSinha-304@users.noreply.github.com>
2020-12-22 22:58:41 -0500

    Add files via upload

---
+++ references.R
@@ -0,0 +1,19 @@
+# References
+
+Banik, Rounak. “The Complete Pokemon Dataset.” Kaggle, 29 Sept. 2017, www.kaggle.com/rounakbanik/pokemon. 
+
+“Base Stats and Battle Stats - Pokemon Sun &amp; Pokemon Moon Wiki Guide.” IGN, 9 Nov. 2016, www.ign.com/wikis/pokemon-sun-pokemon-moon/Base_Stats_and_Battle_Stats. 
+
+David. “How to Make Beautiful Tables in R.” R For the Rest of Us, 12 Nov. 2019, rfortherestofus.com/2019/11/how-to-make-beautiful-tables-in-r/. 
+
+“Serebii.net Games - Capturing Pokmon.” Serebii.net Header, www.serebii.net/games/capture.shtml. 
+
+“The Official Pokémon Website: Pokemon.com: Explore the World of Pokémon.” The Official Pokémon Website | Pokemon.com | Explore the World of Pokémon, www.pokemon.com/us/. 
+
+"The R Project for Statistical Computing", https://www.r-project.org/ 
+  
+  Wickham, Hadley. "ggplot2: Elegant Graphics for Data Analysis", Springer-Verlag New York, 2016. isbn 978-3-319-24277-4, https://ggplot2.tidyverse.org
+
+Wu, Changbao and Mary E. Thompson, "Sampling Theory and Practice", Springer
+
+Zhu, Hao. Create Awesome HTML Table with Knitr::Kable and KableExtra, 22 Oct. 2020, cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_html.html. 
